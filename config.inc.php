64a1e351e3397052bfe50bb2d2461d382d61c5f2
Tyler Folsom <tfolsom@assetvision>
2019-01-06 19:09:16 -0600

    Altered config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -19,19 +19,23 @@ else
 		
 		$domainParts = explode('.', $_SERVER['SERVER_NAME']); //split domain name
 		$configFile = 'config.'.$domainParts[0].'.inc.php'; //include subdomain in config file name
+		$configFile = 'config.'. 'otis' .'.inc.php'; //include subdomain in config file name
 		
 		if ($domainParts[0] == 'boh') {
 			$configPath = '/home/ajswanso/public_html/otis/';
 		} else {
-			$configPath = '/home/ajswanso/public_html/otis/' . $domainParts[0] . '/';
+			//$configPath = '/home/ajswanso/public_html/otis/' . $domainParts[0] . '/';
+			$configPath = '/var/www/html/otis' . $domainParts[0] . '/';
+			$configPath = '/var/www/html/otis/';
 		//	$configPath = '/home/ajswanso/public_html/' . $domainParts[0] . '/';
 
 			//$configPath = '/home/ajswanso/public_html/otis/alclair/';
 		}
-		echo "<br/> Server name is " . $configPath;
+		//echo "<br/> Config Path is " . $configPath;
+		//echo "<br/> Config File is " . $configFile;
 		//If the config file exists, include it.  Include the dev config file if the config file does not exist.
 		if (file_exists($configPath . $configFile)) {
-			echo "<br/> Path is" . $configPath . $configFile;
+			//echo "<br/> Path is" . $configPath . $configFile;
 			include_once($configPath . $configFile);
 		} else {
 			include_once('config.dev.inc.php');

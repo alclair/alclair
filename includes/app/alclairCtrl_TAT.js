c1c051fc8314f88a34934e6761379c47b40c3f6b
Tyler Folsom <tfolsom@assetvision>
2019-05-09 10:41:00 -0500

    Added TAT for Repairs

--- includes/app/alclairCtrl_TAT.js
+++ includes/app/alclairCtrl_TAT.js
@@ -232,6 +232,241 @@ swdApp.controller('TAT', ['$http', '$scope', 'AppDataService', '$upload',  '$coo
     $scope.init();
 	}]);
 	
+swdApp.controller('TAT_Repair', ['$http', '$scope', 'AppDataService', '$upload',  '$cookies', function ($http, $scope, AppDataService, $upload, $cookies) {
+	$scope.OrdersList = {};
+    $scope.PageIndex = 1;
+    $scope.PageSize = window.cfg.PageSize;
+    $scope.TotalPages = 0;
+    $scope.PageWindowSize = 10;
+    $scope.TotalRecords = 0;
+    $scope.SearchText = "";
+	$scope.entityName = "Traveler";
+	$scope.printed_or_not = '0';
+    
+    //SearchStartDate = "10/1/2017";
+    //SearchEndDate = new Date();
+
+    
+    $(function(){
+		$('.press-enter').keypress(function(e){
+    		if(e.which == 13) {
+				//dosomething
+				$(".js-new").first().focus();
+				//alert('Enter pressed');
+    		}
+  		})
+	});
+    
+    $scope.qc_form = {
+        cust_name: '',
+        pass_or_fail: '0',
+        qc_date: new Date,
+    };
+
+	
+    if (window.cfg.Id > 0)
+        $scope.PageIndex = window.cfg.Id;
+	$scope.openStart = function ($event) {        
+        $scope.openedStart = true;
+    };
+	$scope.openEnd = function ($event) {        
+        $scope.openedEnd = true;
+    };
+	$scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+    $scope.disabled = function (date, mode) {
+        return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+    };
+
+    $scope.formats = ['MM/dd/yyyy'];
+    $scope.format = $scope.formats[0];
+	$scope.SearchStartDate=window.cfg.CurrentMonthFirstDate;//OctoberOne;
+	$scope.SearchEndDate=window.cfg.CurrentDay;
+	console.log("Date is " + window.cfg.OctoberOne)
+	console.log("Date2 is " + window.cfg.CurrentMonthFirstDate)
+	//$scope.SearchEndDate=window.cfg.CurrentMonthLastDate;
+    $scope.ClearSearch=function()
+    {
+        $scope.SearchText = "";
+        $scope.cust_name = "";
+		$scope.qc_form = {
+	        cust_name: "",
+    		};
+        $scope.PageIndex = 1;
+        $scope.LoadData();
+        $cookies.put("SearchText", "");
+    }
+    $scope.LoadData = function () {
+        myblockui();
+        //$cookies.put("SearchText", $scope.SearchText);
+        //$cookies.put("SearchText", $scope.cust_name);
+        
+		//$cookies.put("SearchStartDate",$scope.SearchStartDate);
+		//$cookies.put("SearchEndDate",$scope.SearchEndDate);
+		console.log("Date is " + $scope.SearchStartDate)
+		//$scope.SearchStartDate = '3/5/2018';
+		
+        var api_url = window.cfg.apiUrl + "alclair/get_turn_around_time_repairs.php?PageIndex=" + $scope.PageIndex + "&PageSize=" + $scope.PageSize + "&StartDate="+moment($scope.SearchStartDate).format("MM/DD/YYYY")+"&EndDate="+moment($scope.SearchEndDate).format("MM/DD/YYYY");
+        //alert(api_url);
+        $http.get(api_url)
+            .success(function (result) {
+	            console.log("Test1 is " + JSON.stringify(result.test1))
+	            console.log("Test2 is " + JSON.stringify(result.test2))
+	            //console.log("Test3 is " + JSON.stringify(result.test3))
+	            console.log("Counter is " + (result.test4))
+	             
+	             console.log("Min is " + result.min + " Max is " + result.max + " Avg is " + result.avg + " Total is " + result.num_of_orders1 + " and " + result.num_of_repairs2 + " median is " + result.median)
+	            
+	            if(result.min == 0 ) {
+		            $scope.there_is_a_null = 1;
+	            } else {
+		            $scope.there_is_a_null = 0;
+	            }
+	            $scope.min = result.min;
+	            $scope.max = result.max;
+	            $scope.avg = result.avg;
+	            $scope.mode = result.mode;
+	            $scope.median = result.median;
+	            $scope.total_repairs = result.num_of_repairs1;
+	            //console.log("Data is " + JSON.stringify(result.data)))
+                $scope.RepairsList = result.data;
+                $scope.RepairsList_zero = result.data_zero;
+                //console.log("Name is " + $scope.OrdersList[0].designed_for)
+                
+                //$scope.QC_Form = result.customer_name;
+                $scope.TotalPages = result.TotalPages;
+                console.log("Num of pages " + result.TotalPages)
+                console.log("Delete this " + result.test)
+                $scope.TotalRecords = result.TotalRecords;
+                $scope.TotalRecordsActive = result.TotalRecordsActive;
+                $scope.Printed = result.Printed;
+                //console.log("Pass or Fail is " + result.testing1)
+
+                $scope.PageRange = [];
+                $scope.PageWindowStart = (Math.ceil($scope.PageIndex / $scope.PageWindowSize) - 1) * $scope.PageWindowSize + 1;
+                $scope.PageWindowEnd = $scope.PageWindowStart + $scope.PageWindowSize - 1;
+                if ($scope.PageWindowEnd > $scope.TotalPages) {
+                    $scope.PageWindowEnd = $scope.TotalPages;
+                }
+                for (var i = $scope.PageWindowStart; i <= $scope.PageWindowEnd; i++) {
+                    $scope.PageRange.push(i);
+                }
+
+                $.unblockUI();
+            }).error(function (result) {
+                toastr.error("Get QC Form error.");
+            });
+    };
+
+    $scope.GoToPage = function (v) {
+        $scope.PageIndex = v;
+        $scope.LoadData();
+    };
+
+    $scope.Search = function () {        
+        $scope.PageIndex = 1;
+        $scope.LoadData();
+    };
+    
+    $scope.Excel = function () {
+	   myblockui();
+      
+	   	var api_url = window.cfg.apiUrl + "export/alclair_excel_export_tat.php?&StartDate=" + moment($scope.SearchStartDate).format("MM/DD/YYYY") + "&EndDate=" + moment($scope.SearchEndDate).format("MM/DD/YYYY")
+        $http.get(api_url).success(function (result) {
+            $.unblockUI();     
+            $scope.data =  result.data;
+            console.log("TEST IS " + result.test)
+            toastr.success("The Excel document was sent to your e-mail.");
+            console.log("Data is " + JSON.stringify(result.data));
+            console.log("Name is " + JSON.stringify(result.data2));
+            console.log("Code is " + JSON.stringify(result.code));
+        }).error(function (result) {
+            $.unblockUI();
+            toastr.error("Get error.");
+        });
+    };
+
+
+
+    $scope.deleteForm = function (id) {
+        console.log(id);
+        if (confirm("Are you sure you want to delete this repair?") == false) {
+            return;
+        }
+
+        $http.get(window.cfg.apiUrl + "alclair/deleteRepairForm.php?id=" + id).success(function (result) {
+            for (var i = 0; i < $scope.OrdersList.length; i++) {
+                if ($scope.OrdersList[i].id == id) {
+                    toastr.success("Delete Repair successful!", "Message");
+                    $scope.OrdersList.splice(i, 1);
+                    $scope.TotalRecords = $scope.TotalRecords - 1;
+                    break;
+                }
+            }
+        }).error(function (result) {
+            toastr.error("Failed to delete form, please try again.");
+        });
+    };
+    
+    $scope.OpenWindow=function(filepath)
+	{
+		window.open(window.cfg.rootUrl+'/data/'+filepath,'Invoice #'+$scope.customer_name,'width=760,height=600,menu=0,scrollbars=1');
+	}
+       
+    $scope.init = function () {
+	    
+	   
+        //$scope.SearchText = $cookies.get("SearchText");
+        //$scope.cust_name = $cookies.get("SearchText");
+        //$scope.qc_form.cust_name = $cookies.get("SearchText");
+        if (isEmpty($scope.SearchText)) $scope.SearchText = "";
+        if (isEmpty($scope.cust_name)) $scope.cust_name = "";
+        if (isEmpty($scope.qc_form.cust_name)) $scope.qc_form.cust_name = "";
+        $scope.PASS_OR_FAIL = AppDataService.PASS_OR_FAIL;
+        AppDataService.loadMonitorList(null, null, function (result) {
+           $scope.monitorList = result.data;
+        }, function (result) { });
+        AppDataService.loadBuildTypeList(null, null, function (result) {
+           $scope.buildTypeList = result.data;
+        }, function (result) { });
+        AppDataService.loadOrderStatusTableList(null, null, function (result) {
+           $scope.orderStatusTableList = result.data;
+        }, function (result) { });
+   		/*if($cookies.get("SearchStartDate")!=undefined)
+		{
+			$scope.SearchStartDate=moment($cookies.get("SearchStartDate")).format("MM/DD/YYYY");
+		}
+		if($cookies.get("SearchEndDate")!=undefined)
+		{
+			$scope.SearchEndDate=moment($cookies.get("SearchEndDate")).format("MM/DD/YYYY");
+		}*/
+        $scope.LoadData();
+    }
+    			    
+    $scope.PDF = function (id) {
+	    //var $order_id = 9729;
+        
+        //myblockui();
+        console.log("Order num is " +  id)
+        var api_url = window.cfg.apiUrl + "alclair/travelerPDF.php?ID=" + id;
+
+        $http.get(api_url).success(function (result) {
+            $.unblockUI();
+            console.log("Result is " + result.data);
+
+            window.open(window.cfg.rootUrl + "/data/exportpdf/" + result.data);
+        }).error(function () {
+            $.unblockUI();
+            toastr.error("Get error.");
+        });
+    }
+    
+    $scope.init();
+	}]);
+		
+	
 	swdApp.controller('Pipeline', ['$http', '$scope', 'AppDataService', '$upload',  '$cookies', function ($http, $scope, AppDataService, $upload, $cookies) {
 	$scope.OrdersList = {};
     $scope.PageIndex = 1;

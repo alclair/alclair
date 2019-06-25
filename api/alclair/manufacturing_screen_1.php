d1da467d43bca815fafacf18a962eb6a64a4e9ff
Tyler Folsom <tfolsom@assetvision>
2019-06-25 15:00:37 -0500

    Updated Phil's Page for Zeeshan

--- api/alclair/manufacturing_screen_1.php
+++ api/alclair/manufacturing_screen_1.php
@@ -364,6 +364,28 @@ ORDER BY date_done ASC,  t1.import_orders_id";
     $stmt = pdo_query( $pdo, $query, $params); 
     $result = pdo_fetch_all( $stmt );
     $response['orders_shipped_yesterday'] = count($result);
+    
+    $minus_1_day = date("m/d/Y 00:00:00", strtotime("-1 days"));
+    $today_midnight = date("m/d/Y 00:00:00");
+    $minus_1_minute = date("m/d/Y 23:59:59", strtotime("-1 days"));
+    $today_almost_tomorrow = date("m/d/Y 23:59:59");
+	
+    $conditionSql.=" and (t1.date>=:BeginningOfDay)";
+	$params[":BeginningOfDay"]=$today_midnight;  //date("m/d/Y H:i:s",strtotime($_REQUEST["EndDate"] . '00:00:00'));
+	$conditionSql.=" and (t1.date<=:EndOfDay)";
+	$params[":EndOfDay"]=$today_almost_tomorrow;  //date("m/d/Y H:i:s",strtotime($_REQUEST["EDate"] . '23:59:59'));
+
+    
+    $query = "SELECT DISTINCT t1.import_orders_id, t2.designed_for, t3.status_of_order, t2.order_id AS order_id, to_char(t1.date, 'MM/dd/yyyy    HH24:MI') as date_done, t4.name AS model, t1.order_status_id, t1.notes,  t2.id
+FROM order_status_log AS t1
+LEFT JOIN import_orders AS t2 ON t1.import_orders_id = t2.id
+LEFT JOIN order_status_table AS t3 ON 12 = t3.order_in_manufacturing
+LEFT JOIN monitors AS t4 ON t2.model = t4.name
+WHERE t1.order_status_id = 12 AND t2.active = TRUE $conditionSql  AND t4.name IS NOT NULL
+ORDER BY date_done ASC,  t1.import_orders_id";
+    $stmt = pdo_query( $pdo, $query, $params); 
+    $result = pdo_fetch_all( $stmt );
+    $response['orders_shipped_today'] = count($result);
     		
 	$response["num_of_repairs1"] = $ind;
 	$response["num_of_repairs2"] = count($difference);

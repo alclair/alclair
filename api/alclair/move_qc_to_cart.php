9b13964bdc3f5ecbb6d5340602af650d9fa1a64f
Tyler Folsom <tfolsom@assetvision>
2020-01-29 20:25:33 -0600

    Completed orig order ship date & failed QC to cart

--- api/alclair/move_qc_to_cart.php
+++ api/alclair/move_qc_to_cart.php
@@ -24,9 +24,35 @@ try
 	$stmt = pdo_query($pdo, "SELECT * FROM qc_form WHERE id = :id", array(":id"=>$qc_id));
 	$get_stmt = pdo_fetch_all( $stmt );
 	$id_of_order = $get_stmt[0]["id_of_order"];
+	$id_of_repair = $get_stmt[0]["id_of_repair"];
 	
 	$stmt = pdo_query( $pdo,  'UPDATE qc_form SET notes = :notes WHERE id = :id', array("id"=>$qc_id, ":notes"=>$notes));
-	$stmt = pdo_query( $pdo,  'UPDATE import_orders SET order_status_id = :order_status_id WHERE id = :id', array(":id"=>$id_of_order, ":order_status_id"=>$order_status_id));
+	
+	if($get_stmt[0]["id_of_order"] != NULL) {
+		$stmt = pdo_query( $pdo,  'UPDATE import_orders SET order_status_id = :order_status_id WHERE id = :id', array(":id"=>$id_of_order, ":order_status_id"=>$order_status_id));
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
+		//$query = pdo_query($pdo, "SELECT * FROM order_status_table WHERE status_of_order = 'Digital Impression Detailing'", null);
+		//$result = pdo_fetch_array($query);
+		//$status_id = $result["order_in_manufacturing"];
+			
+		// ORDER STATUS LOG
+		// IMPORT ORDERS			
+		$stmt = pdo_query( $pdo, "INSERT INTO order_status_log (date, import_orders_id, order_status_id, notes,  user_id) VALUES (now(), :import_orders_id, :status_id, :notes, :user_id) RETURNING id", array(':import_orders_id'=>$id_of_order, ':status_id'=>$order_status_id, ':notes'=>$notes, ':user_id'=>$_SESSION['UserId']));					 					 
+		$rowcount = pdo_rows_affected( $stmt );
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	} else {
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+		//$query = pdo_query($pdo, "SELECT * FROM repair_status_table WHERE status_of_repair = 'Quality Control'", null);
+		//$result = pdo_fetch_array($query);
+		//$status_id = $result["order_in_repair"];
+		$stmt = pdo_query( $pdo,  'UPDATE repair_form SET repair_status_id = :repair_status_id WHERE id = :id', array(":id"=>$id_of_repair, ":repair_status_id"=>$order_status_id));
+		// ORDER STATUS LOG
+		// IMPORT ORDERS			
+		$stmt = pdo_query( $pdo, "INSERT INTO repair_status_log (date, repair_form_id, repair_status_id, notes,  user_id) VALUES (now(), :repair_form_id, :status_id, :notes, :user_id) RETURNING id", array(':repair_form_id'=>$id_of_repair, ':status_id'=>$order_status_id, ':notes'=>$notes, ':user_id'=>$_SESSION['UserId']));					 					 
+			 
+		$rowcount = pdo_rows_affected( $stmt );
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	}
 	
 	$response['code'] = 'success';
 	$response['data'] = $rowcount;

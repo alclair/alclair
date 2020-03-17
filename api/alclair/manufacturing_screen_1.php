443f1876acd0eaa94af17c37ffe353cbb1a708eb
Tyler Folsom <tfolsom@assetvision>
2020-03-17 15:31:31 -0500

    Updated Order Counts for Done

--- api/alclair/manufacturing_screen_1.php
+++ api/alclair/manufacturing_screen_1.php
@@ -101,11 +101,13 @@ try
     $query2 = "SELECT count(t1.id) FROM order_status_log AS t1
     					LEFT JOIN import_orders AS t2 ON t1.import_orders_id = t2.id
 						LEFT JOIN order_status_table AS t3 ON 12 = t3.order_in_manufacturing
-						WHERE t1.order_status_id = 12 AND t2.active = TRUE AND t1.date > '$january_current_year' AND t1.date < '$december_current_year' AND (t2.customer_type='Customer' OR t2.customer_type IS NULL)";
+						LEFT JOIN monitors AS t4 ON t2.model = t4.name
+						WHERE t1.order_status_id = 12 AND t2.active = TRUE AND t4.name IS NOT NULL AND t1.date > '$january_current_year' AND t1.date < '$december_current_year' AND (t2.customer_type='Customer' OR t2.customer_type IS NULL)";
 
     $stmt2 = pdo_query( $pdo, $query2, null); 
     $result2 = pdo_fetch_array( $stmt2 );
     $response['Shipped_This_Year'] = $result2[0];
+    
 	 
 	 // SHIPPED THIS YEAR THIS MONTH
     //$beginning_of_month_current_year = $current_month . '/01/' . $current_year;

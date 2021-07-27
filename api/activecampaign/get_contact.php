15fa00b4679cbc4d8d9bdd90cb7939b7e6249197
Tyler Folsom <tfolsom@assetvision>
2021-07-27 11:34:51 -0500

    Order by impression received date in Manufacturing Screens

---
+++ api/activecampaign/get_contact.php
@@ -0,0 +1,38 @@
+<?php
+include_once "../../config.inc.php";
+if(empty($_SESSION["UserId"]))
+{
+    return;
+}
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://youraccountname.api-us1.com/api/3/contacts?status=-1&orders[andy@alclair.com]=ASC",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Accept: json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$response["test"] = "HERE";
+echo json_encode($response);
+exit;
+
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+?>

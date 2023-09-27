bdae5feb562182a13829cbec7a7aff7dcc6be5df
Tyler Folsom <tfolsom@assetvision>
2023-09-27 16:01:36 -0400

    Added Last Scan

--- api/WooCommerce/01_get_woocommerce_orders_copy.php
+++ api/WooCommerce/01_get_woocommerce_orders_copy.php
@@ -122,8 +122,8 @@ $before = $yesterday_year . "-" . $yesterday_month . "-" . $yesterday_day . "T23
 
 
 $params = [
-			'before' => '2023-09-18T23:59:59',
-			'after' => '2023-09-18T00:00:00',
+			'before' => '2023-09-26T23:59:59',
+			'after' => '2023-09-26T00:00:00',
 			'per_page' => 100
         ];
 
